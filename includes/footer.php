83f0ca58e209e85a1e63aa6d5b80bf26e56005e4
Abhijeet Karmaker <37630628+abhikarmaker@users.noreply.github.com>
2018-09-23 14:46:14 -0400

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,18 @@
+﻿<link rel="stylesheet" href="css/style.css">
+<footer id="footer">
+  <div class="container">
+    <div class="row">
+      <div class="grid_12">
+		<center><p>Copyright © All Rights Reserved <a href="index.php">www.dorotahouse.co.in</a></p></center>
+		 <center> FE-163, Sector - III, Salt Lake,Kolkata - 700106, West bengal </center>
+		 <center>Ph no: +91 9836177779</center>
+		 <center>For Reservation : <a href="mailto:reservations@dorota.co.in?Subject=Enquiry%20" target="_top">reservations@dorota.co.in </a></center>
+		
+<div class="socials">
+          <a href="https://www.facebook.com/Dorot a-599042796956737/" class="fa fa-facebook"></a>
+          <a href="https://www.airbnb.co.in/rooms" class="fa fa-airbnb"><img src=".\images\airbnb.png"> </a>
+          </div>
+      </div>
+    </div>
+  </div>  
+  </footer>
